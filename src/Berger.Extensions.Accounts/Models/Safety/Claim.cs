5eb72bdd357efd3f6800f3565de8cab27dc10a79
Rafael Cronemberger <rafael@berger.com.br>
2024-01-10 10:43:45 -0300

    Package updates

--- src/Berger.Extensions.Accounts/Models/Safety/Claim.cs
+++ src/Berger.Extensions.Accounts/Models/Safety/Claim.cs
@@ -6,7 +6,7 @@ namespace Berger.Extensions.Accounts
     {
         #region Properties
         public Guid OwnerId { get; set; }
-        public Guid? ModelId { get; set; }
+        public Guid? EntityId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Slug { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
